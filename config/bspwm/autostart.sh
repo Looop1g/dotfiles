b0a40acbe1b8c2019dc6081b891ec7dc1d6d27da
looopig <zxcvbnm.an94@gmail.com>
2023-05-16 18:23:09 +0800

    Update the configuration of bspwm

--- config/bspwm/autostart.sh
+++ config/bspwm/autostart.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-pgrep -x picom > /dev/null || picom --config $HOME/.config/picom/picom.conf &
-pgrep -x fcitx5 > /dev/null || fcitx5 &
+pgrep -x picom >/dev/null || picom --config $HOME/.config/picom/picom.conf &
+pgrep -x fcitx5 >/dev/null || fcitx5 &
+pgrep -x clash >/dev/null || clash &
+pgrep -x flameshot >/dev/null || flameshot &
+pgrep -x gentoo-pipewire-launcher >/dev/null || gentoo-pipewire-launcher &
+
+sleep 10
 pgrep -x dropbox >/dev/null || dropbox &
-pgrep -x clash > /dev/null || clash &
-pgrep -x flameshot > /dev/null || flameshot &
