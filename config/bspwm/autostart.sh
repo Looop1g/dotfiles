c7c55dcabfe77da41b62ba28dc9bebc896549be4
looopig <zxcvbnm.an94@gmail.com>
2023-06-25 00:30:08 +0800

    更改开机自启动的 clash 命令

--- config/bspwm/autostart.sh
+++ config/bspwm/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pgrep -x picom >/dev/null || picom --config $HOME/.config/picom/picom.conf &
 pgrep -x fcitx5 >/dev/null || fcitx5 &
-pgrep -x clash >/dev/null || clash &
+pgrep -x cfw >/dev/null || cfw &
 pgrep -x flameshot >/dev/null || flameshot &
 pgrep -x gentoo-pipewire-launcher >/dev/null || gentoo-pipewire-launcher &
 
