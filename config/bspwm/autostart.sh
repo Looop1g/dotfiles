0d689401eb3e53c69a026a2a253da7f49e08ee0e
Looop1g <zxcvbnm.an94@gmail.com>
2024-12-14 12:40:37 +0800

    Updater files CONFIG

--- config/bspwm/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pgrep -x picom >/dev/null || picom --config $HOME/.config/picom/picom.conf &
-pgrep -x fcitx5 >/dev/null || fcitx5 &
-pgrep -x cfw >/dev/null || cfw &
-pgrep -x flameshot >/dev/null || flameshot &
-pgrep -x gentoo-pipewire-launcher >/dev/null || gentoo-pipewire-launcher &
-
-sleep 10
-pgrep -x dropbox >/dev/null || dropbox &
--- config/polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-while [ ! -z "$(pgrep polybar)" ]; do
-	killall polybar
-done
-
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-polybar -q main -c "$HOME/.config/polybar/config.ini"
